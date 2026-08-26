db48c57a72ad408fb471057d8f10795b62d514d1
abdalla-shaker <abdallashaker143@gmail.com>
2026-08-26 19:21:39 +0300

    feat: add weather details section and loading state
    Implemented the weather details UI
    Added details for feels like, humidity, wind, and precipitation
    Added the loading state for the weather details section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./components/header/Header.jsx";
 import Hero from "./components/hero/Hero.jsx";
+import Main from "./components/main/Main.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+
+      <div className="weather-details-container">
+        <Main />
+        <aside>
+          <h2>Hourly forecast</h2>
+        </aside>
+      </div>
     </>
   );
 }
